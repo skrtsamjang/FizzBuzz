571c8abe07720b9fba67a3fabfe12fbf425c3a8f
Hamin Lee <daniehm0503@gmail.com>
2023-09-11 16:05:35 -0400

    refactored to extract helper method

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -32,3 +32,42 @@ class FizzBuzz {
         }
     }
 }
+
+class FizzBuzzWhile {
+    public static void main(String[] args) {
+
+        int i = 1;
+
+        while (i < 100) {
+
+            i = doFizzBuzz(i);
+
+        }
+
+    }
+
+    private static int doFizzBuzz(int i) {
+        boolean divisibleBy3 = i % 3 == 0;
+        boolean divisibleBy5 = i % 5 == 0;
+
+        if (divisibleBy3 && divisibleBy5) {
+
+            System.out.println("Fizz Buzz");
+
+        } else if (divisibleBy3) {
+
+            System.out.println("Fizz");
+
+        } else if (divisibleBy5) {
+
+            System.out.println("Buzz");
+
+        } else {
+            System.out.println(i);
+        }
+
+        i = i + 1;
+        return i;
+    }
+}
+
