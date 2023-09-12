2057766a4c46b6834de2ab2fd2c814e8a4eb7d44
Hamin Lee <daniehm0503@gmail.com>
2023-09-12 15:20:30 -0400

    task 4 done

--- src/Multiples.java
+++ src/Multiples.java
@@ -7,12 +7,12 @@ public class Multiples {
         for (int i=1000; i > 0; i--) {
 
             if (i % 3 == 0) {
-                count3 = count3 + 1;
+                count3 += 1;
             }
             if (i % 5 == 0) {
-                count5 = count5 + 1;
+                count5 += 1;
             }
         }
-        System.out.println(count3+count5);
+        System.out.println(count5 + count3);
     }
 }
